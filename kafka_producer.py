69bdd8f70035b56436a9c247649fb838e7ee936d
Kyler Dana <kymasterbox@gmail.com>
2024-12-18 12:57:54 -0700

    Producer and Lists Topics. Needs data_processor using Spark to work

--- kafka_producer.py
+++ kafka_producer.py
@@ -3,22 +3,27 @@ import requests
 import json
 import time
 
-API_KEY = 'your_api_key'
-STOCK_SYMBOL = 'TSLA'
+API_KEY = 'HEBOH95078GF5MAS'  # Replace with your API key
+STOCK_SYMBOL = 'AAPL'
 KAFKA_TOPIC = 'stock_prices'
 KAFKA_SERVER = 'localhost:9092'
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER,
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_SERVER,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
 
 while True:
-    response = requests.get(f'https://www.alphavantage.co/query',
-                            params={
-                                'function': 'TIME_SERIES_INTRADAY',
-                                'symbol': STOCK_SYMBOL,
-                                'interval': '1min',
-                                'apikey': API_KEY
-                            })
+    response = requests.get(
+        'https://www.alphavantage.co/query',
+        params={
+            'function': 'TIME_SERIES_INTRADAY',
+            'symbol': STOCK_SYMBOL,
+            'interval': '1min',
+            'apikey': API_KEY
+        }
+    )
     data = response.json()
     producer.send(KAFKA_TOPIC, value=data)
+    print("Data sent to Kafka")
     time.sleep(60)
